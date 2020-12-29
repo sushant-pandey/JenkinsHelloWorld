5cec594770aa4f3bf4a499f74e40c80c96839070
sushant-pandey <sushant0985@gmail.com>
2020-12-28 19:49:22 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("git webhook here *******Compiling Java file from command line and then with Jenkins*******");
+		System.out.println("git webhook here *******Jenkins*******");
 	}	
 }
